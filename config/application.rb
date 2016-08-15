db01c3eca51639b0bc21cdcba2b6060542db1671
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-08-15 15:08:24 -0400

    Done.

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ Bundler.require(*Rails.groups)
 
 module RailsProgrammerProfilePartials
   class Application < Rails::Application
+  config.web_console.development_only = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
